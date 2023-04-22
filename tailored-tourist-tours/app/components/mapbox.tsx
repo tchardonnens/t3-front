965aa8ce7b807cf23d52c56b0659ce28a9372557
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-04-22 16:13:54 +0200

    adjust map view height

--- tailored-tourist-tours/app/components/mapbox.tsx
+++ tailored-tourist-tours/app/components/mapbox.tsx
@@ -1,21 +1,32 @@
-import Map, { GeolocateControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import Map, {
+  GeolocateControl,
+} from "react-map-gl";
 
 export default function MapboxMap() {
+  const mapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN
   return (
+
     <div>
       <Map
-        mapboxAccessToken="MAPBOX_ACCESS_TOKEN"
+        mapboxAccessToken="pk.eyJ1IjoidG9tNzgiLCJhIjoiY2xnbTkwYnQ0MDNkbzNsb2V3d3hvd3Q2diJ9.efwRWl4ud5nylL_H3kuhIQ"
         initialViewState={{
-          longitude: -100,
-          latitude: 40,
-          zoom: 3.5,
+          longitude: 2.33,
+          latitude: 48.86,
+          zoom: 10,
+        }}
+        style={{
+          width: "80vw",
+          height: "80vh",
+          borderRadius: "15px",
+          border: "1px solid black",
         }}
         mapStyle="mapbox://styles/mapbox/streets-v11"
       >
         <GeolocateControl
           positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
+
         />
       </Map>
     </div>
--- tailored-tourist-tours/app/page.tsx
+++ tailored-tourist-tours/app/page.tsx
@@ -3,11 +3,8 @@ import MapboxMap from "./components/mapbox";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <h1 className="text-6xl font-bold">Tailored Tourist Tours</h1>
-      <p className="text-2xl font-light">
-        A curated list of the best tourist tours in the world.
-      </p>
       <div className="flex flex-col items-center justify-center w-full h-full">
         <MapboxMap />
       </div>
