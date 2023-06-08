dc56cea59c9e4c5a9b0c948d45cbb1702132feb4
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-06-08 22:04:44 +0200

    better error handling + better UI

--- t3/app/page.tsx
+++ t3/app/page.tsx
@@ -4,8 +4,12 @@ import Form from "./components/form";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
-      <h1 className="text-5xl font-bold mb-10">T3 🌎</h1>
+      <h1 className="text-3xl md:text-5xl font-bold mb-2">Tailored tours 🇫🇷</h1>
+      <p className="text-xl mb-10">Plan your next visits in France!</p>
       <Form />
+      <footer className="mt-auto">
+        <b><a href="https://thomascdnns.com">Made by Thomas Chardonnens ✌️</a></b>
+      </footer>
     </main>
   )
 }
